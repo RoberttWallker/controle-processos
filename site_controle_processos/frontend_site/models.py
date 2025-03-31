356b04e6233a5a5f38208c834a049538f787f0dd
RoberttWallker <robertwalker.rodrigues@gmail.com>
2025-03-31 17:36:11 -0300

    Modificação do modelo de lancamento de lentes e template de lançamento.

--- site_controle_processos/frontend_site/models.py
+++ site_controle_processos/frontend_site/models.py
@@ -4,10 +4,31 @@ from django.contrib.auth.models import User, AbstractUser
 # Create your models here.
 
 class ComprasLentes(models.Model):
-    descricao = models.TextField(max_length=255, blank=False, null=False)
-    nota_fiscal = models.IntegerField(blank=False, null=False)
-    valor_custo = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
     data_compra = models.DateField(blank=False, null=False)
-    sequencial = models.IntegerField(blank=False, null=False)
-    referencia_fabricante = models.TextField(max_length=100, blank=False, null=False)
+    ordem_servico = models.TextField(max_length=50, blank=False, null=False)
+    sequencial_savwin = models.IntegerField(blank=False, null=False)
+    codigo = models.IntegerField(blank=False, null=False)
+    descricao_lente = models.TextField(max_length=255, blank=False, null=False)
+    referencia_fabricante = models.TextField(max_length=100, blank=True, null=True)
+    numero_pedido = models.IntegerField(blank=False, null=False)
+    custo_site = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False, default=0.00)
+    data_liberacao_blu = models.DateField(blank=False, null=False)
+    valor_pago = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False, default=0.00)
+    custo_tabela = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False, default=0.00)
+    nota_fiscal = models.IntegerField(blank=False, null=False)
+    custo_nota_fiscal = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False, default=0.00)
+    duplicata = models.IntegerField(blank=False, null=False)
     observacao = models.TextField(max_length=600, blank=True, null=True)
+    usuario_criacao = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='compras_criadas')
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    usuario_atualizacao = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='compras_atualizadas')
+    data_atualizacao = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if 'request' in kwargs:
+            request = kwargs.pop('request')
+            if request.user.is_authenticated:
+                if not self.pk:  # Se for um novo registro
+                    self.usuario_criacao = request.user
+                self.usuario_atualizacao = request.user  # Sempre atualiza o último usuário que alterou
+        super().save(*args, **kwargs)
